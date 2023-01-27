08bceff0feaa04e3760cca74018e5b45801aa756
Timo Derstappen <teemow@gmail.com>
2023-01-27 22:13:17 +0100

    added a few details to the readme

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # ble-midi-footswitch
+
 An ESP32 (Wemos Lolin32) based footswitch controller 
+
+Material:
+
+ * Aluminium box (1590DD)
+ * Wemos Lolin32
+ * Wemos LED Matrix 8x8
+ * Wemos prototype shield
+ * USB-B socket to micro USB
+ * 2 MIDI sockets (5 pin)
+ * 6 10mm LEDs
+ * 6 10mm LED sockets
+ * 6 momentary footswitches
+
+## Photos
+
+![](https://github.com/teemow/ble-midi-footswitch/blob/main/docs/photos/raw_case.png?raw=true)
+![](https://github.com/teemow/ble-midi-footswitch/blob/main/docs/photos/raw_case_with_holes.png?raw=true)
+![](https://github.com/teemow/ble-midi-footswitch/blob/main/docs/photos/raw_case_with_holes_back.png?raw=true)
+![](https://github.com/teemow/ble-midi-footswitch/blob/main/docs/photos/testing_ble_with_a_switch.mp4?raw=true)
+![](https://github.com/teemow/ble-midi-footswitch/blob/main/docs/photos/painted_case.png?raw=true)
+![](https://github.com/teemow/ble-midi-footswitch/blob/main/docs/photos/fully_assembled.png?raw=true)
+![](https://github.com/teemow/ble-midi-footswitch/blob/main/docs/photos/fully_assembled_back.png?raw=true)
+![](https://github.com/teemow/ble-midi-footswitch/blob/main/docs/photos/testing_leds.mp4?raw=true)
+![](https://github.com/teemow/ble-midi-footswitch/blob/main/docs/photos/testing_the_matrix.mp4?raw=true)
+![](https://github.com/teemow/ble-midi-footswitch/blob/main/docs/photos/first_full_test.mp4?raw=true)
+
--- ble-midi-footswitch.ino
+++ ble-midi-footswitch.ino
@@ -13,21 +13,11 @@ AutoConnect Portal(Server);
 #include <BLEMIDI_Transport.h>
 #include <hardware/BLEMIDI_ESP32_NimBLE.h>
 
-// LED pins
-const byte led_blue_1 = 13;
-const byte led_blue_2 = 12;
-const byte led_green_1 = 14;
-const byte led_green_2 = 27;
-const byte led_red_1 = 26;
-const byte led_red_2 = 25;
+// LED pins (red, red, green, green, blue, blue)
+const int led_pins[6] = {26, 25, 14, 27, 13, 12};
 
 // Button pins
-const int btn_blue_1 = 15;
-const int btn_blue_2 = 2;
-const int btn_green_1 = 0;
-const int btn_green_2 = 4;
-const int btn_red_1 = 16;
-const int btn_red_2 = 17;
+const int btn_pins[6] = {16, 17, 0, 4, 15, 2};
 
 // Connect to first server found
 BLEMIDI_CREATE_INSTANCE("ThreeFoot", MIDI);
@@ -41,20 +31,15 @@ void readCB(void *parameter);
 
 bool isConnected = false;
 
-int btn_state_blue_1;
-int btn_state_blue_2;
-int btn_state_green_1;
-int btn_state_green_2;
-int btn_state_red_1;
-int btn_state_red_2;
-int btn_last_state_blue_1;
-int btn_last_state_blue_2;
-int btn_last_state_green_1;
-int btn_last_state_green_2;
-int btn_last_state_red_1;
-int btn_last_state_red_2;
+int btn_states[6] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH};
+int btn_last_states[6] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH};
 
-int pixel = 0;
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long btn_last_debounce_times[6] = {0, 0, 0, 0, 0, 0};
+unsigned long debounceDelay = 30;
+
+int bank = 1;
 
 // 8x8 led matrix
 #define D5 18 // SCK - Clock for 8x8 matrix
@@ -67,76 +52,43 @@ int pixel = 0;
 
 MLED matrix(7);
 
-void drawEyes(boolean winkL, boolean winkR) {
-  // left eye
-  mled.dot(1,6, winkL ? 0:1 );
-  mled.dot(2,6, winkL ? 0:1 );
-  mled.dot(1,5);
-  mled.dot(2,5);
-
-  //right eye
-  mled.dot(5,6, winkR ? 0:1);
-  mled.dot(5,5);
-  mled.dot(6,6, winkR ? 0:1);
-  mled.dot(6,5);
-}
-
-  smile_bmp[] =
-  { B00111100,
-    B01000010,
-    B10100101,
-    B10000001,
-    B10100101,
-    B10011001,
-    B01000010,
-    B00111100 },
-
-void drawHappyMouth() {
-  mled.dot(1,2);
-  mled.dot(2,1);
-  mled.dot(3,1);
-  mled.dot(4,1);
-  mled.dot(5,1);
-  mled.dot(6,2);
-}
-
-void drawAngryMouth() {
-  mled.dot(1,1);
-  mled.dot(2,2);
-  mled.dot(3,2);
-  mled.dot(4,2);
-  mled.dot(5,2);
-  mled.dot(6,1);
-}
-
 static const uint8_t PROGMEM
+  heart_bmp[] = 
+  { B00000000,
+    B01100110,
+    B10011001,
+    B10000001,
+    B01000010,
+    B00100100,
+    B00011000,
+    B00000000 },
   smile_bmp[] =
-  { B00111100,
+  { B00000000,
+    B01100110,
+    B01100110,
+    B00000000,
+    B00000000,
     B01000010,
-    B10100101,
-    B10000001,
-    B10100101,
-    B10011001,
+    B00111100,
+    B00000000 },
+  wink_bmp[] =
+  { B00000000,
+    B01100000,
+    B01100110,
+    B00000000,
+    B00000000,
     B01000010,
-    B00111100 },
-  neutral_bmp[] =
-  { B00111100,
-    B01000010,
-    B10100101,
-    B10000001,
-    B10111101,
-    B10000001,
-    B01000010,
-    B00111100 },
+    B00111100,
+    B00000000 },
   frown_bmp[] =
-  { B00111100,
+  { B00000000,
+    B00000000,
+    B01100110,
+    B00000000,
+    B00000000,
+    B00111100,
     B01000010,
-    B10100101,
-    B10000001,
-    B10011001,
-    B10100101,
-    B01000010,
-    B00111100 };
+    B00000000 };
 
 void setup() {
   Serial.begin(115200);
@@ -144,20 +96,14 @@ void setup() {
   clearMatrix();
 
   // initialize button pins
-  pinMode(btn_blue_1, INPUT_PULLUP);
-  pinMode(btn_blue_2, INPUT_PULLUP);
-  pinMode(btn_green_1, INPUT_PULLUP);
-  pinMode(btn_green_2, INPUT_PULLUP);
-  pinMode(btn_red_1, INPUT_PULLUP);
-  pinMode(btn_red_2, INPUT_PULLUP);
+  for (int i = 0; i < 6; i++) {
+    pinMode(btn_pins[i], INPUT_PULLUP);
+  }
   
   // initialize LED pins
-  pinMode(led_blue_1, OUTPUT);
-  pinMode(led_blue_2, OUTPUT);
-  pinMode(led_green_1, OUTPUT);
-  pinMode(led_green_2, OUTPUT);
-  pinMode(led_red_1, OUTPUT);
-  pinMode(led_red_2, OUTPUT);
+  for (int i = 0; i < 6; i++) {
+    pinMode(led_pins[i], OUTPUT);
+  }
 
   MIDI.begin(MIDI_CHANNEL_OMNI);
 
@@ -165,30 +111,24 @@ void setup() {
     Serial.println("---------CONNECTED---------");
     isConnected = true;
     
-    drawSmileFace();
-
+    drawSmile();
     blinkLEDBlue(50);
     vTaskDelay(50/portTICK_PERIOD_MS);
     blinkLEDBlue(50);
-
     vTaskDelay(850/portTICK_PERIOD_MS);
-
-    clearMatrix();
+    drawBank();
   });
 
   BLEMIDI.setHandleDisconnected([]() {
     Serial.println("---------NOT CONNECTED---------");
     isConnected = false;
 
-    drawFrownFace();
-    
+    drawFrown();
     blinkLEDRed(50);
     vTaskDelay(50/portTICK_PERIOD_MS);
     blinkLEDRed(50);
-
     vTaskDelay(850/portTICK_PERIOD_MS);
-    
-    clearMatrix();
+    drawBank();
   });
 
   MIDI.setHandleNoteOn([](byte channel, byte note, byte velocity) {
@@ -223,19 +163,18 @@ void setup() {
   blinkLEDGreen(100);
   blinkLEDBlue(100);
 
-  drawNeutralFace();
+  drawSmile();
   delay(1000);
-
-  // wink right
-  drawSmileFace();
+  drawWink();
   delay(1000);
-
-  clearMatrix();
+  drawHeart();
+  delay(1000);
+  drawBank();
 
   /*
   Server.on("/", rootPage);
   AutoConnectConfig config;
-  config.apid = "THREE";
+  config.apid = +"THREE";
   config.psk = "THR33!!!";
   Portal.config(config);
   Portal.begin();
@@ -243,38 +182,37 @@ void setup() {
 }
 
 void loop() {
-  btn_last_state_blue_1 = btn_state_blue_1;
-  btn_last_state_blue_2 = btn_state_blue_2;
-  btn_last_state_green_1 = btn_state_green_1;
-  btn_last_state_green_2 = btn_state_green_2;
-  btn_last_state_red_1 = btn_state_red_1;
-  btn_last_state_red_2 = btn_state_red_2;
-  btn_state_blue_1 = digitalRead(btn_blue_1);
-  btn_state_blue_2 = digitalRead(btn_blue_2);
-  btn_state_green_1 = digitalRead(btn_green_1);
-  btn_state_green_2 = digitalRead(btn_green_2);
-  btn_state_red_1 = digitalRead(btn_red_1);
-  btn_state_red_2 = digitalRead(btn_red_2);
 
-  if (btn_last_state_blue_1 == 1 && btn_state_blue_1 == 0) {
-    sendControlChange(60, led_blue_1);
-  }
-  if (btn_last_state_blue_2 == 1 && btn_state_blue_2 == 0) {
-    sendControlChange(61, led_blue_2);
-  }
-  if (btn_last_state_green_1 == 1 && btn_state_green_1 == 0) {
-    sendControlChange(62, led_green_1);
-  }
-  if (btn_last_state_green_2 == 1 && btn_state_green_2 == 0) {
-    sendControlChange(63, led_green_2);
-  }
-  if (btn_last_state_red_1 == 1 && btn_state_red_1 == 0) {
-    sendControlChange(64, led_red_1);
-  }
-  if (btn_last_state_red_2 == 1 && btn_state_red_2 == 0) {
-    sendControlChange(65, led_red_2);
+  // upper red is bank up
+  if (buttonPressed(0)) {
+    bank++;
+    if (bank > 9) {
+      bank = 9;
+    }
+    digitalWrite(led_pins[0], HIGH);
+    drawBank();
+    digitalWrite(led_pins[0], LOW);
   }
 
+  // lower red is bank down
+  if (buttonPressed(1)) {
+    bank--;
+    if (bank < 1) {
+      bank = 1;
+    }
+    digitalWrite(led_pins[1], HIGH);
+    drawBank();
+    vTaskDelay(250/portTICK_PERIOD_MS);
+    digitalWrite(led_pins[1], LOW);
+  }
+
+  // all others send midi cc
+  for (int i = 2; i < 6; i++) {
+    if (buttonPressed(i)) {
+      sendControlChange(58 + i + (bank - 1) * 4, led_pins[i]);
+    }
+  }
+  
   //Portal.handleClient();
 }
 
@@ -303,51 +241,72 @@ void sendControlChange(int note, byte led) {
     vTaskDelay(250/portTICK_PERIOD_MS);
     digitalWrite(led, LOW);
   } else {
-    drawFrownFace();
+    drawFrown();
 
     blinkAllLEDs(10);
     vTaskDelay(10/portTICK_PERIOD_MS);
     blinkAllLEDs(10);
 
     vTaskDelay(500/portTICK_PERIOD_MS);
-    clearMatrix();
+    drawBank();
   }
 }
 
-void blinkLEDBlue1(int ms) {
-  digitalWrite(led_blue_1, HIGH);
-  vTaskDelay(ms/portTICK_PERIOD_MS);
-  digitalWrite(led_blue_1, LOW);
-}
+bool buttonPressed(int i) {
+  int reading = digitalRead(btn_pins[i]);
 
-void blinkLEDBlue2(int ms) {
-  digitalWrite(led_blue_2, HIGH);
-  vTaskDelay(ms/portTICK_PERIOD_MS);
-  digitalWrite(led_blue_2, LOW);
-}
+  // reset the debouncing timer
+  if (reading != btn_last_states[i]) {
+    btn_last_debounce_times[i] = millis();
+  }
+  btn_last_states[i] = reading;
 
-void blinkLEDGreen1(int ms) {
-  digitalWrite(led_green_1, HIGH);
-  vTaskDelay(ms/portTICK_PERIOD_MS);
-  digitalWrite(led_green_1, LOW);
-}
+  if ((millis() - btn_last_debounce_times[i]) > debounceDelay) {
+    if (reading != btn_states[i]) {
+      btn_states[i] = reading;
 
-void blinkLEDGreen2(int ms) {
-  digitalWrite(led_green_2, HIGH);
-  vTaskDelay(ms/portTICK_PERIOD_MS);
-  digitalWrite(led_green_2, LOW);
+      if (btn_states[i] == LOW) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 void blinkLEDRed1(int ms) {
-  digitalWrite(led_red_1, HIGH);
+  digitalWrite(led_pins[0], HIGH);
   vTaskDelay(ms/portTICK_PERIOD_MS);
-  digitalWrite(led_red_1, LOW);
+  digitalWrite(led_pins[0], LOW);
 }
 
 void blinkLEDRed2(int ms) {
-  digitalWrite(led_red_2, HIGH);
+  digitalWrite(led_pins[1], HIGH);
   vTaskDelay(ms/portTICK_PERIOD_MS);
-  digitalWrite(led_red_2, LOW);
+  digitalWrite(led_pins[1], LOW);
+}
+
+void blinkLEDGreen1(int ms) {
+  digitalWrite(led_pins[2], HIGH);
+  vTaskDelay(ms/portTICK_PERIOD_MS);
+  digitalWrite(led_pins[2], LOW);
+}
+
+void blinkLEDGreen2(int ms) {
+  digitalWrite(led_pins[3], HIGH);
+  vTaskDelay(ms/portTICK_PERIOD_MS);
+  digitalWrite(led_pins[3], LOW);
+}
+
+void blinkLEDBlue1(int ms) {
+  digitalWrite(led_pins[4], HIGH);
+  vTaskDelay(ms/portTICK_PERIOD_MS);
+  digitalWrite(led_pins[4], LOW);
+}
+
+void blinkLEDBlue2(int ms) {
+  digitalWrite(led_pins[5], HIGH);
+  vTaskDelay(ms/portTICK_PERIOD_MS);
+  digitalWrite(led_pins[5], LOW);
 }
 
 void blinkLEDBlue(int ms) {
@@ -376,21 +335,33 @@ void clearMatrix() {
   matrix.writeDisplay();
 }
 
-void drawSmileFace() {
+void drawBank() {
+  matrix.clear();
+  matrix.drawChar(1, 1, bank + 48, 1, 0, 1);
+  matrix.writeDisplay();
+}
+
+void drawSmile() {
   matrix.clear();
   matrix.drawBitmap(0, 0, smile_bmp, 8, 8, LED_ON);
   matrix.writeDisplay();
 }
 
-void drawNeutralFace() {
+void drawWink() {
   matrix.clear();
-  matrix.drawBitmap(0, 0, neutral_bmp, 8, 8, LED_ON);
+  matrix.drawBitmap(0, 0, wink_bmp, 8, 8, LED_ON);
   matrix.writeDisplay();
 }
 
-void drawFrownFace() {
+void drawFrown() {
   matrix.clear();
   matrix.drawBitmap(0, 0, frown_bmp, 8, 8, LED_ON);
+  matrix.writeDisplay();
+}
+
+void drawHeart() {
+  matrix.clear();
+  matrix.drawBitmap(0, 0, heart_bmp, 8, 8, LED_ON);
   matrix.writeDisplay();
 }
 
